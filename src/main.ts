e17ac47ed3378b9374aa598159fe55b1237d4404
saish26 <113912090+saish26@users.noreply.github.com>
2023-05-26 07:02:29 +0545

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+// import express from "express"
+
+// const app = express()
+
+// // respond with "hello world" when a GET request is made to the homepage
+// app.get("/", (req, res) => {
+//   res.send("hello world")
+// })
+
+// app.listen(3000)
+
+const hello = (x: any) => {
+  console.log('hi', x);
+};
+hello('ss');
+console.log('hello there');
+
+const x = 'hello';
